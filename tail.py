04f0a1665f7b2fad7039126d0e79b4bde39f193d
Diego.S <diego.schyns@gmail.com>
2024-11-22 15:19:52 +0100

    tail part 1 Co-authored-by: Leo78578 <Leo78578@users.noreply.github.com>

--- tail.py
+++ tail.py
@@ -8,11 +8,32 @@ Group member:
 
 import numpy as np
 import matplotlib as plt
+###Variables a changer car pas sur des données#############
 
-V_h = 56
-S = 164.9 #m² 
-MAC = 4.622 #m
-b = 29 #m
+V_h = 0.74 #m^3 horizontal tail volume
+V_v=0.071 #m^3 vertical tail volume
+S = 164.9 #m² area of the wing 
+MAC = 4.622 #m mean chord of the line 
+b = 29 #m span
 
-def tail(x_h,x_c):
-    return 
+#NACA 0012 for vertical parts of the tail
+def tail(x_h,x_v):
+
+    #calcul des surfaces d une tail conventionnel 
+    S_h=V_h*S*MAC/(x_h)
+    S_v=V_v*S*b/x_v
+    S_tot_tail=np.sqrt(S_v**2+S_h**2)
+    #calcul du butterfly angle
+    gamma_h=np.arctan(S_v/S_h)
+    #Calcul de l AR de la tail
+
+    b_tail=5 #m longueur de la queue
+    AR_tail=b_tail**2/S_tot_tail
+    
+    taper_ratio_tail=0.5
+    c_tail_root=2.5 # m chord a la racine de la tail horizontal 
+    c_tail_tip=1.5 #m chord a la fin de la tail horizontal 
+    sweep_angle_tail=0.726632927 #radians 
+    c_v_tail=2*S/(b(1+taper_ratio_tail)) #tail volume coefficient
+
+    return gamma_h
