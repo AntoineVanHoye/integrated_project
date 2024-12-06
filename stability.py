7898ef75ebef81deb3b96446e6d341d6c973ecbb
Léo <ldipuma@hotmail.be>
2024-12-06 20:26:10 +0100

    dur...

--- stability.py
+++ stability.py
@@ -15,8 +15,10 @@ CL_tail = data_tail[:,1]
 Cm_wings = data_wings[:,4]
 CL_wings = data_wings[:,1]
 
-a = (CL_wings[68]- CL_wings[67])/(AoA_wings[68]-AoA_wings[67])
-a1 = (CL_tail[68]- CL_tail[67])/(AoA_tail[68]-AoA_tail[67])
+#a = (CL_wings[68]- CL_wings[67])/(AoA_wings[68]-AoA_wings[67])*np.pi/180
+#a1 = (CL_tail[68]- CL_tail[67])/(AoA_tail[68]-AoA_tail[67])
+a = 3.211
+a1 = 2
 
 a1_over_a = a1/a
 
@@ -27,13 +29,14 @@ AoA_tip = 3
 AoA_wing = (AoA_root - AoA_tip)/2
 Cm_fus_cruise = -0.1072 #at 0° AoA
 Cm_wings_cruise =  -0.1448 #at AoA_wing
-CL_wing = 0.675
-CL_fus = 0.059
-CL_T = 7
+CL_wing = 0.574 #total wing lift coeff in cruise 
+CL_fus = 0.056
+CL_T = 0.03
 
+"""
 #calculate the forces 
-S_fus = 110.67
-S_wing = 54.22
+
+
 rho = 0.288
 Mach = 0.9
 R = 287                 #[m^2/s^2K]
@@ -41,24 +44,32 @@ gamma = 1.4
 T = 216.5
 speed_sound = np.sqrt(gamma * R * T) #speed of sound 
 V = speed_sound * Mach
-wing_AR = 6.021
+
 
 L_wing = CL_wing * 1/2 * rho* S_wing * V**2
 L_fus = CL_fus * 1/2 * rho* S_fus* V**2
 L_T = 5
-
-x_AC_wing = 2
-x_CG_plane = 11
-x_AC_fus = 5
-x_AC_tail = 7
-y_AC_wing = 3
-y_AC_tail = 4
-MAC_wing = 8
-S_T = 2
+"""
+S_wing = 66.44
+wing_AR = 6.021
+#values with respect to the airplane nose 
+x_AC_wing = 11.91
+x_CG_plane = 10.4
+x_AC_fus = 1.33
+x_AC_tail = 16.5
+z_AC_wing = 0
+z_AC_tail = 1
+MAC_tail = 1.483
+S_T = 5.422
+MAC_wing = 4.141
+MAC_fus = 13.305
+fus_length = 16.8
+fus_width = 9
+S_fus = 110.67
 
 #tail volume ratio effectivness 
 
-V_T = S_T * (x_AC_tail - x_CG_plane)/(S_wing * MAC_wing)
+V_T = S_T * (x_AC_tail - x_CG_plane)/(S_wing * MAC_tail)
 
 def plot_CL(AoA, CL): 
     plt.plot(AoA, CL, color = 'b')
@@ -70,8 +81,8 @@ def plot_CL(AoA, CL):
 
     return
 
-plot_CL(AoA_wings,CL_wings)
-plot_CL(AoA_tail, CL_tail)
+#plot_CL(AoA_wings,CL_wings)
+#plot_CL(AoA_tail, CL_tail)
 
 #to see the trend of Cm with respect to the angle of attack
 def plot_Cm(AoA_fus, AoA_wings, Cm_fus, Cm_wings):
@@ -95,18 +106,18 @@ def plot_Cm(AoA_fus, AoA_wings, Cm_fus, Cm_wings):
 
     return
 
-plot_Cm(AoA_fus, AoA_wings,Cm_fus,Cm_wings)
+#plot_Cm(AoA_fus, AoA_wings,Cm_fus,Cm_wings)
 
 ##################################################################
 ######EQUILIBRIUM IN PITCH
 ##################################################################
 
 def equilibrium() : 
-    Cm_tot = Cm_fus_cruise + 2* Cm_wings_cruise + 2 * CL_wing* (x_AC_wing - x_CG_plane) + CL_fus * (x_AC_fus - x_CG_plane) - 2*CL_T *V_T
+    Cm_tot = Cm_fus_cruise + 2*Cm_wings_cruise + CL_wing* (x_CG_plane - x_AC_wing)/MAC_wing + CL_fus * (x_CG_plane - x_AC_fus)/MAC_fus - 2*CL_T *V_T
 
     #check the equilibrium at cruise
 
-    if Cm_tot == 0 : 
+    if Cm_tot > -1e-3 and Cm_tot < 1e-3 : 
         print("The airplane is at equilibrium because Cm_tot = 0")
     
     else : 
@@ -122,24 +133,31 @@ equilibrium()
 
 def downwash():
     It = x_AC_tail - x_AC_wing
-    lamb = 0.137
-    b = 29
-    m = (y_AC_tail - y_AC_wing)/b*2
+    lamb = 0.161 #taper ratio of the wing 
+    b = 29 #span
+    m = (z_AC_tail - z_AC_wing)/b*2 #because (z_AC_tail - z_AC_wing) = mb/2
 
-    deps = (1.75*a)/(np.pi*wing_AR*((2*lamb*It)/b)**1.4*(1 + m))
+    deps = (1.75*a)/(np.pi*wing_AR*((2*lamb*It)/b)**1.4*(1 + np.abs(m)))
     
     return deps
 
 eps = downwash()
+print("deps/dalpha is equal to", eps)
+
 
 def long_stat_stab(): #in the pitching plane
     #check the stability
+    #neutral point : position of the cg in order to have the derivative equals 0
     deps = downwash()
-    hn = x_AC_wing + 2*V_T*a1_over_a*(1- deps) #position of the neutral point 
-    derivative = hn - x_CG_plane 
+    h0 = (x_AC_fus + x_AC_wing)/2
+ 
+    hn = h0 + 2*V_T*a1_over_a*(1- deps) - 0.5*fus_width**2 * fus_length/(S_wing*a*MAC_wing)#position of the neutral point  
+
+    MAC = (MAC_wing + MAC_fus)/2
+    derivative = (x_CG_plane - hn)/MAC
     Kn = - derivative #static margin
 
-    if Kn >= 0.05 : 
+    if Kn >= 0.05 and Kn < 0.15 : 
         print("The static margin has a correct value and is equal to : ", (Kn*100))
 
     else : 
