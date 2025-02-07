70f83c8098a86ec17f69dd82bc8207a96ad564c9
gllmv <115586141+gllmv@users.noreply.github.com>
2025-02-07 15:28:53 +0100

    landing gear 07/02

--- landing_gear.py
+++ landing_gear.py
@@ -4,8 +4,8 @@ import math
 import initmass as init
 
 #geometry
-x_cg = 10.4                 # coordinate in x of the center of gravity (estimate). Y coordinate will for now assumed to be on the chord
-y_cg = 0                    # coordinate in y of the center of gravity (estimate)
+x_cg = 9.4                 # coordinate in x of the center of gravity (estimate). Y coordinate will for now assumed to be on the chord
+y_cg = -0.5                    # coordinate in y of the center of gravity (estimate)
 Vstall = 52.57              # stalling velocity with flaps(m/s)
 Cl0 = 0.244                 # lift coefficient at 0 degree angle
 a = 1.754                   # Cl derivative in alpha
@@ -14,8 +14,9 @@ S = 204.41                  # Surface of the wing m^2
 x_fus = 16.8                # lenght of fuselage
 
 #takeoff general data
-theta_LOF = 15                  # maximum pitch angle margin to take before takeoff
-theta_TOA =                     # turnover angle
+theta_LOF = 15               # maximum pitch angle margin to take before takeoff
+theta_margin = 2
+theta_TOA = 25              # turnover angle
 S = 1829                        # maximum airplane runway (less than 1800 m idealy)
 rhoair_grnd_20 = 1.395          # air density on the ground kg/m3 at 20° Fahrenheit
 rhoair_grnd_85 = 1.146          # air density on the ground kg/m3 at 85° Fahrenheit
@@ -24,18 +25,6 @@ w_naft = 0.08                       # weight fraction on the forward lg at takeo
 w_maft = 1 - w_naft                    # weight fraction on the rear lg at takeoff
 w_nfwd = 0.15                   # weight fraction on the forward lg for the most forward position of cg
 w_mfwd = 1 - w_nfwd             # weight fraction on the rear lg for the most forward position of cg
-'''
-C_r = 4.622
-C_t = 1.448
-#C_R=  (S)*(C_r-C_t)/9
-
-M = 2/3*(C_r + C_t-(C_r*C_t)/(C_r+C_t)) #for the standard straight tapered wing
-#M = 1/2 * (C_r+C_t)
-b = 20
-H = b/8*(C_r**2+2*C_r*C_t+3*C_t**2)/(C_r**2+C_r*C_t+C_t**2)
-
-print(M,H)
-'''
 
 #Determination of key variables
 
@@ -49,8 +38,8 @@ print(M,H)
 
 V_LOF = 1.1 * Vstall        # Lift-off speed(emprirical formula)
 
-Cl_20 = 2*(init.W0*1000000/453592*g)/(rhoair_grnd_20*V_LOF**2*S)    # Cl at lift-off for 20° / 85°
-Cl_85 = 2*(init.W0*1000000/453592*g)/(rhoair_grnd_85*V_LOF**2*S)    
+Cl_20 = 2*(init.W0*453592/1000000*g)/(rhoair_grnd_20*V_LOF**2*S)    # Cl at lift-off for 20° / 85°
+Cl_85 = 2*(init.W0*453592/1000000*g)/(rhoair_grnd_85*V_LOF**2*S)    
 
 
 AlphaLOF_20 = (Cl_20-Cl0)/(a)*180/np.pi      # Angle of attack at lift-off for 20° / 85°
@@ -83,13 +72,14 @@ transla_b = np.ones((len(x))) *7.794
 x_t, y_t = points[:, 0]*1.448 + transla , points[:, 1]*1.448 #draw the rear wing limit
 x_b, y_b = points[:, 0]*9.006+ transla_b , points[:, 1]*9.006
 
-H = (x_t[-1]-x_cg-np.tan(np.pi/2-np.radians(theta_LOF))*(y_cg + y_t[-1]))/(np.tan(np.radians(theta_LOF))+np.tan(np.pi/2-np.radians(theta_LOF)))
-h = H * np.tan(np.radians(theta_LOF))
+H = (x_t[-1]-x_cg-np.tan(np.pi/2-np.radians(theta_LOF))*(-y_cg + y_t[-1]))/(np.tan(np.radians(theta_LOF+theta_margin))+np.tan(np.pi/2-np.radians(theta_LOF)))
+h = H * np.tan(np.radians(theta_LOF+theta_margin))
 
 
 x_w, y_w = x_cg + h , y_cg - H
 x_n = x_w -(x_w-x_cg)/w_naft
 y_n = y_w
+print(x_n/16.8)
 x_cgfwd = x_w - w_nfwd *(x_w-x_n)
 y_cgfwd = y_cg
 
@@ -166,15 +156,47 @@ plt.plot(line[0] ,line[1],  marker=None, markersize=2, linestyle='-', color='blu
 plt.plot(line_minus[0] ,line_minus[1],  marker=None, markersize=2, linestyle='-', color='blue')
 
 # Draw cg and landing gear
+z_n = 0
+z_w =z_n + (x_w-x_n)*np.tan(np.arcsin((y_cg-y_w)/(x_cg-x_n)*1/(np.tan(np.radians(theta_TOA)))))
 plt.plot(z_cg,x_cg,marker='o', markersize=7,color= 'red',label ='cg.aft')
 plt.plot(z_n,x_n,marker='o', markersize=7,color= 'black',label ='cg.aft')
-
+plt.plot(z_w,x_w,marker='o', markersize=7,color= 'black',label ='cg.aft')
+plt.plot(-z_w,x_w,marker='o', markersize=7,color= 'black',label ='cg.aft')
 
 plt.title(f"Latteral disposition of the landing gear")
 plt.xlabel("y-[m]")
 plt.ylabel("x-[m]")
 plt.axis("equal")  # Ensure equal scaling on both axes
 plt.grid(True)
+
+
+# Weight determination (statistical)
+
+W_l = (init.W0)
+#method 1
+Kfl = 0.11
+Krf = 0.15
+Ksl = 1
+Klg = Ksl + Krf + Kfl
+
+w_g1 = 0.046*Klg
+w_nose = w_nfwd * w_g1
+w_rear = w_mfwd * w_g1
+print(w_g1,w_nose,w_rear)
+
+
+#method 2 (statist)
+n= 1.17
+Kg = 20.45
+
+#table values
+Kcg = 0.718
+
+w_g2 = Kg * Kcg*(W_l/1000)**n*1/W_l
+w_nose = w_nfwd * w_g1
+w_rear = w_mfwd * w_g1
+print(w_g2,w_nose,w_rear)
+
 
 
 plt.show()
