205129826372c0a551f4ab84e8436ae9d18a6bd5
gllmv <115586141+gllmv@users.noreply.github.com>
2025-02-11 11:07:48 +0100

    cabin volume+ landing 11/02

--- cabin.py
+++ cabin.py
@@ -3,10 +3,4 @@ import math
 import numpy as np
 #Reference dimention (taken from the area of the bombardier cabin)
 
-height = 1.88
-width = 2.43
-
-#print(Total_vol_estim)
-#print(Total_vol_rough)
-Total_area_estim = Total_vol_estim /height
-Total_area_rough = Total_vol_rough /height
+Vc = 124.39361564911602783 # volume cabin in m3  (approx)
