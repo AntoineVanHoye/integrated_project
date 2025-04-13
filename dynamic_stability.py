b3e68f66009a63ff808055f5f18ae0ecf1cd340c
Léo <ldipuma@hotmail.be>
2025-04-13 13:40:53 +0200

    stab_dyn

--- dynamic_stability.py
+++ dynamic_stability.py
@@ -1,13 +1,6 @@
 import numpy as np 
 
 ##################################################################
-######CONFIGURATION SETTING
-##################################################################
-
-config = 3
-fuel = 2
-
-##################################################################
 ######GENERAL PARAMETERS     
 ##################################################################
 
@@ -16,38 +9,41 @@ beta = np.sqrt(1-M**2)
 e = 0.85 # Oswald efficiency factor
 alpha_e = 0*np.pi/180
 delta = 0.005
-rho = 0.28724050871107903
-V0 = 265.5380870986307
-g = 9.81
+rho = 0.28724050871107903*(0.0685218/35.3147) #air density in slugs/ft^3
+V0 = 265.5380870986307*3.28084 #velocity in ft/s
+g = 9.81*3.28084 #gravity in ft/s^2
 CL = 0.45
 CD = 0.028
+CD0 = 0.0012
 
 ##################################################################
 ######WING PARAMETERS      
 ##################################################################
 
 sweep_quarter_wing = 29.0*np.pi/180
-surface_wing_ideal = 119.05
+surface_wing_ideal = 119.05*10.7639 #surface of the wing in ft^2
 sweep_LE_wing = 31.599 *np.pi/180
-surf_wing = 71.11
+surf_wing = 71.11*10.7639
 cl_alpha_wing = 6.680687891225398
 AR_wing = 7.04
 taper_ratio_wing = 0.492
 sweep_half_chord_wing = np.arctan(np.tan(sweep_quarter_wing)+4/AR_wing*(1-taper_ratio_wing)/(1+taper_ratio_wing)*(0.25-0.5))
-MAC_wing = 3.706
+MAC_wing = 3.706*3.28084 #mean aerodynamic chord in ft
+B = np.sqrt(1 - M**2 * np.cos(sweep_quarter_wing)**2)
 
 ##################################################################
 ######FUSELAGE PARAMETERS      
 ##################################################################
 
-surf_fus = 127.07
+surf_fus = 127.07*10.7639
 sweep_LE_fus = 50.0*np.pi/180
 cl_alpha_fus = 5.641342450858086
-b_fus = 9
+b_fus = 9*3.28084
 AR_fus = 0.637
 taper_ratio_fus = 0.681
 sweep_quarter_fus = 41.791 *np.pi/180
 sweep_half_chord_fus = np.arctan(np.tan(sweep_quarter_fus)+4/AR_fus*(1-taper_ratio_fus)/(1+taper_ratio_fus)*(0.25-0.5))
+av_section_fus = 1.792*7.564*10.7639 
 
 ##################################################################
 ######GENERAL GEOMETRICAL PARAMETERS      
@@ -55,32 +51,35 @@ sweep_half_chord_fus = np.arctan(np.tan(sweep_quarter_fus)+4/AR_fus*(1-taper_rat
 
 b = 28.95*3.28084 # Wingspan in ft
 b_wing = b - b_fus
-l_cabin = 10.1
-cabin_width = 9       
-l_cockpit = 2.01
+l_cabin = 10.1*3.28084
+cabin_width = 9*3.28084    
+l_cockpit = 2.01*3.28084
 AR = 7.04
-surf_tot = 198.17
-MAC_tot = 11.098
-x_AC_tot = 8.556
+surf_tot = 198.17*10.7639
+MAC_tot = 11.098*3.28084
+x_AC_tot = 8.556*3.28084
 cl_alpha = (cl_alpha_wing * b_wing + cl_alpha_fus * b_fus) / b
 kappa = cl_alpha/(2*np.pi)
 sweep_half_chord = (sweep_half_chord_fus*surf_fus + sweep_half_chord_wing*surf_wing)/surf_tot
-CL_alpha = 3.03
+CL_alpha = 3.17
 
 ##################################################################
 ######TAIL PARAMETERS
 ##################################################################
 
-z_f = 1.51 #distance between AC of the tail and AC of the aircraft
-surf_vert_tail = 19
-c_root_tail = 3.5
-MAC_tail = 2.5120665203177994
-x_AC_tail_local = 2.660991632261686
+z_f = 1.51*3.28084 #distance between AC of the tail and AC of the aircraft
+surf_vert_tail = 19*10.7639
+c_root_tail = 3.5*3.28084
+MAC_tail = 2.5120665203177994*3.28084
+x_AC_tail_local = 2.660991632261686*3.28084
 x_AC_tail = l_cabin + l_cockpit + x_AC_tail_local + 1
 eta_tail = 3.98*np.pi/180
-hor_tail_surf = 35
+hor_tail_surf = 35*10.7639
 AR_tail = 5.079670344831602
 CL_alpha_tail = 2.745145450926034
+y_AC_tail = 2.8645301924973645*3.28084
+sweep_quarter_tail = 34.89785663924308 * np.pi/180
+z_w = 1*3.28084 #distance from body centerline to quarter chord point of exposed wing root chord, positive for the quarter chord point below the body centerline
 
 ##################################################################
 ######INERTIAS
@@ -89,18 +88,18 @@ CL_alpha_tail = 2.745145450926034
 I_y = 165669.0
 I_x = 165669.0
 I_xz = 165669.0
-I_z = 165669.0
+I_z = 136985.0
 
 ##################################################################
 ######PARAMETERS TO CHANGE
 ##################################################################
 
 Kn = 0.1
-Le = 584000
-m = 17642.0
-x_CG_tot = 8.5
+Le = 584000*0.224809 #lift force in lb
+m = 106000*0.45359237 #mass in kg
+x_CG_tot = 8.5*3.28084
 
-l_f = x_AC_tail - x_CG_tot
+l_V = x_AC_tail - x_CG_tot
 V_tail = hor_tail_surf * (x_AC_tail - x_CG_tot)/(surf_tot* MAC_tot)
 
 ##################################################################
@@ -112,6 +111,15 @@ W_e = V0 * np.sin(alpha_e)
 theta_e = 0
 
 ##################################################################
+######PARAMETERS TO BE OBTAINED FROM THE GRAPHS 
+##################################################################
+
+K = 0.7 #for q derivatives
+k = 0.9 #for v derivatives
+term1 = -0.5
+term2 = -0.4 #for r derivatives
+
+##################################################################
 ######LONGITUDINAL DYNAMIC DERIVATIVES
 ##################################################################
 
@@ -119,7 +127,7 @@ theta_e = 0
 ######ALPHA DERIVATIVES 
 ##################################################################
 
-def alpha_der(i,d):
+def alpha_der():
 
     CL_alpha = 2*np.pi*AR/(2+np.sqrt(4+(AR**2 *beta**2 * kappa**2)*(1 + np.tan(sweep_half_chord)**2/beta**2)))
     CD_alpha = 2*CL*CL_alpha/(np.pi*AR*e)
@@ -127,19 +135,18 @@ def alpha_der(i,d):
 
     return CL_alpha, CD_alpha, CM_alpha
 
-
 ##################################################################
 ######U DERIVATIVES 
 ##################################################################
 
-def u_der1(i,d): 
+def u_der1(): 
 
-    CL_alpha = alpha_der(i,d)[0]
-    CM_alpha = alpha_der(i,d)[2]
+    CL_alpha = alpha_der()[0]
+    CM_alpha = alpha_der()[2]
     CL_M = 0
-    CD_alpha = alpha_der(i,d)[1]
+    CD_alpha = alpha_der()[1]
     CD_M = 0
-    CM_alpha = alpha_der(i,d)[2]
+    CM_alpha = alpha_der()[2]
     x_AC_M = 0
 
     CL_u = 2*CL - alpha_e*CL_alpha + M*CL_M
@@ -148,11 +155,11 @@ def u_der1(i,d):
 
     return CL_u, CD_u, CM_u
 
-def u_der2(i,d): 
+def u_der2(): 
 
-    CL_u = u_der1(i,d)[0]
-    CD_u = u_der1(i,d)[1]
-    CM_u = u_der1(i,d)[2]
+    CL_u = u_der1()[0]
+    CD_u = u_der1()[1]
+    CM_u = u_der1()[2]
 
     Z_u = -CL_u *np.cos(alpha_e) - CD_u *np.sin(alpha_e)
     X_u = CL_u *np.sin(alpha_e) - CD_u* np.cos(alpha_e)
@@ -168,20 +175,17 @@ def u_der2(i,d):
 ######Q DERIVATIVES 
 ##################################################################
 
-def q_der1(i,d):
+def q_der1():
 
-    CL_alpha = alpha_der(i,d)[0]   
+    CL_alpha = alpha_der()[0]   
     l_T = x_AC_tail - x_CG_tot
     X_W = np.abs(x_AC_tot - x_CG_tot)
-    b = 28.95*3.28084
     mean_chord = AR_tail/b
 
     CL_q_wing_M0 = (1/2 + 2*X_W/mean_chord)*CL_alpha
-    B = np.sqrt(1 - M**2 * np.cos(sweep_quarter_wing)**2)
     CL_q_wing = (AR_tail + 2*np.cos(sweep_quarter_wing))/(AR_tail*B + 2*np.cos(sweep_quarter_wing))*CL_q_wing_M0
     CL_q_tail = 2*CL_alpha_tail*eta_tail * V_tail
 
-    K = 0.7
     CM_q_wing_M0 = -K * cl_alpha_wing * np.cos(sweep_quarter_wing) * ((AR_tail*(2 * (X_W/MAC_wing)**2 + 1/2 *(X_W/MAC_wing)))/(AR + 2*np.cos(sweep_quarter_wing)) + 1/24 * (AR_tail**3 * np.tan(sweep_quarter_wing)**2)/(AR_tail + 6*np.cos(sweep_quarter_wing) + 1/8))
     CM_q_wing = (((AR_tail**3 * np.tan(sweep_quarter_wing)**2)/(AR_tail*B + 6*np.cos(sweep_quarter_wing)) + 3/B)/(((AR_tail**3 * np.tan(sweep_quarter_wing)**2)/(AR_tail*B + 6*np.cos(sweep_quarter_wing))) + 3)) * CM_q_wing_M0
     CM_q_tail = -2*CL_alpha_tail*eta_tail*V_tail*l_T/MAC_tail
@@ -192,11 +196,11 @@ def q_der1(i,d):
 
     return CL_q, CD_q, CM_q
 
-def q_der2(i,d):
+def q_der2():
 
-    CL_q = q_der1(i,d)[0]
-    CD_q = q_der1(i,d)[1]
-    CM_q = q_der1(i,d)[2]
+    CL_q = q_der1()[0]
+    CD_q = q_der1()[1]
+    CM_q = q_der1()[2]
 
     Z_q = -1/2*(CL_q*np.cos(alpha_e) + CD_q*np.sin(alpha_e))
     X_q = 1/2*(CL_q*np.sin(alpha_e) - CD_q*np.cos(alpha_e))
@@ -212,7 +216,7 @@ def q_der2(i,d):
 ######ALPHA DOT DERIVATIVES 
 ##################################################################
 
-def alpha_dot_der(i,d):
+def alpha_dot_der():
 
     deps_dalpha = 0 #approximation
     l_T = x_AC_tail - x_CG_tot
@@ -232,11 +236,11 @@ def alpha_dot_der(i,d):
 ######W DOT DERIVATIVES 
 ##################################################################
 
-def w_dot_der(i,d): 
+def w_dot_der(): 
 
-    CL_alpha_dot = alpha_dot_der(i,d)[0]
-    CD_alpha_dot = alpha_dot_der(i,d)[1]
-    CM_alpha_dot = alpha_dot_der(i,d)[2]
+    CL_alpha_dot = alpha_dot_der()[0]
+    CD_alpha_dot = alpha_dot_der()[1]
+    CM_alpha_dot = alpha_dot_der()[2]
 
     X_w_dot = 1/(2*np.cos(alpha_e)) * (CL_alpha_dot*np.sin(alpha_e) - CD_alpha_dot*np.cos(alpha_e))
     Z_w_dot = 1/(2*np.cos(alpha_e)) * (-CL_alpha_dot*np.cos(alpha_e) - CD_alpha_dot*np.sin(alpha_e))
@@ -252,11 +256,11 @@ def w_dot_der(i,d):
 ######W DERIVATIVES 
 ##################################################################
 
-def w_der(i,d):
+def w_der():
 
-    CL_alpha = alpha_der(i,d)[0]
-    CD_alpha = alpha_der(i,d)[1]
-    CM_alpha = alpha_der(i,d)[2]
+    CL_alpha = alpha_der()[0]
+    CD_alpha = alpha_der()[1]
+    CM_alpha = alpha_der()[2]
    
     De = CD*1/2*rho*V0**2*surface_wing_ideal
     Xe = Le*np.sin(alpha_e) - De*np.cos(alpha_e)
@@ -279,23 +283,23 @@ def w_der(i,d):
 ######CONSTRUCTION OF THE A MATRIX FOR THE LONGITUDINAL MOTION
 ##################################################################
 
-def long_dyn_stab(i,d,Cl): 
+def long_dyn_stab(): 
 
-    X_w_dot = w_dot_der(i,d)[0]
-    Z_w_dot = w_dot_der(i,d)[1]
-    M_w_dot = w_dot_der(i,d)[2]
+    X_w_dot = w_dot_der()[0]
+    Z_w_dot = w_dot_der()[1]
+    M_w_dot = w_dot_der()[2]
 
-    X_u = u_der2(i,d)[1]
-    Z_u = u_der2(i,d)[0]
-    M_u = u_der2(i,d)[2]
+    X_u = u_der2()[1]
+    Z_u = u_der2()[0]
+    M_u = u_der2()[2]
 
-    X_w = w_der(i,d)[1]
-    Z_w = w_der(i,d)[0]
-    M_w = w_der(i,d)[2]
+    X_w = w_der()[1]
+    Z_w = w_der()[0]
+    M_w = w_der()[2]
 
-    X_q = q_der2(i,d)[1]
-    Z_q = q_der2(i,d)[0]
-    M_q = q_der2(i,d)[2]
+    X_q = q_der2()[1]
+    Z_q = q_der2()[0]
+    M_q = q_der2()[2]
     
     M_matrix = np.array([
         [m, -X_w_dot, 0, 0],
@@ -324,48 +328,51 @@ def long_dyn_stab(i,d,Cl):
     else:
         test = 0
     
-    return real_parts, test
+    return real_parts, test, eigenvalues
 
 ##################################################################
 ######LATERAL DYNAMIC DERIVATIVES
 ##################################################################
 
-def dsigma_dbeta(sweep_quarter_wing,AR): 
-
-    average_fus_sect = 0
-    d = np.sqrt(average_fus_sect/0.7854)
-    Z_w = 0#vertical distance from the wing root quarter-chord to the fuselage centerline  
-    d_sigma = -0.276 + 3.06*surf_vert_tail/surf_tot /(1 + np.cos(sweep_quarter_wing)) + 0.4*Z_w + 0.009*AR
-    return d_sigma
-
 ##################################################################
 ######V DERIVATIVES 
 ##################################################################
 
-def v_der(sweep_quarter_wing,AR):
+def v_der():
 
-    d_sigma = dsigma_dbeta(sweep_quarter_wing,AR)
-    c_1 = 0#fin lift curve slope
-    Y_v = 1/2*rho*V0**2 *surf_vert_tail * c_1 * (1-d_sigma)
-    L_v = -1/2 * rho*V0**2 * surf_vert_tail * z_f * c_1 * (1-d_sigma)
-    N_v = 1/2 * rho*V0*surf_vert_tail * l_f * c_1 * (1-d_sigma)
+    d = np.sqrt(av_section_fus/0.7854)
+    term = 0.724 * 3.06*(surf_vert_tail/surface_wing_ideal)/(1+np.cos(sweep_quarter_tail))+0.4*z_w/d + 0.009*AR
 
-    Y_v = Y_v * (1/2*rho*V0*surface_wing_ideal)
+    Y_v = -k*CL_alpha_tail * term * surf_vert_tail/surface_wing_ideal
+    L_v = Y_v * (y_AC_tail*np.cos(alpha_e) - l_V*np.sin(alpha_e))/b
+    N_v = -Y_v * (l_V*np.cos(alpha_e) + y_AC_tail*np.sin(alpha_e))/b
+
+    Y_v_norm = Y_v * (1/2*rho*V0*surface_wing_ideal)
     L_v = L_v * (1/2*rho*V0*surface_wing_ideal*b)
     N_v = N_v * (1/2*rho*V0*surface_wing_ideal*b)
 
-    return Y_v, L_v, N_v
+    return Y_v, L_v, N_v, Y_v_norm
 
 ##################################################################
 ######R DERIVATIVES 
 ##################################################################
 
-def r_der(sweep_quarter_wing,AR):
+def r_der():
 
-    Y_v = v_der(sweep_quarter_wing,AR)[0]
-    Y_r = -2*Y_v * (z_f * np.sin(alpha_e) - l_f * np.cos(alpha_e)/b)
-    L_r = -2*Y_v*((z_f*np.sin(alpha_e) + l_f*np.cos(alpha_e)*(z_f*np.cos(alpha_e) - l_f*np.sin(alpha_e)))/b**2)
-    N_r = 2*Y_v * ((z_f*np.sin(alpha_e + l_f*np.cos(alpha_e))**2)/b**2)
+    Y_v = v_der()[0]
+    Y_r = -2*Y_v * (y_AC_tail * np.sin(alpha_e) - l_V * np.cos(alpha_e)/b)
+    """
+    L_rW = CL * (1+(((AR*(1-B**2)/(2*B*(AR*B + 2*np.cos(sweep_quarter_wing))))+ (((AR*B+2*np.cos(sweep_quarter_wing)/(AR*B+4*np.cos(sweep_quarter_wing))*np.tan(sweep_quarter_wing)**2/8)))))/(1 + ((AR+2*np.cos(sweep_quarter_wing)/(AR+4*np.cos(sweep_quarter_wing))*np.tan(sweep_quarter_wing)**2/8))))
+    L_rV = -2/b**2*(l_V * np.cos(alpha_e)+y_AC_tail*np.sin(alpha_e))*(y_AC_tail*np.cos(alpha_e - l_V*np.sin(alpha_e)))*Y_v
+    L_r = L_rW + L_rV
+    """
+    L_r = -2*Y_v*((z_f*np.sin(alpha_e) + l_V*np.cos(alpha_e)*(z_f*np.cos(alpha_e) - l_V*np.sin(alpha_e)))/b**2)
+    """
+    N_rV = 2/b**2*(l_V*np.cos(alpha_e)+y_AC_tail*np.sin(alpha_e))**2*Y_v
+    N_rW = term1 * CL**2 + term2*CD0
+    N_r = N_rW + N_rV
+    """
+    N_r = 2*Y_v * ((z_f*np.sin(alpha_e + l_V*np.cos(alpha_e))**2)/b**2)
 
     Y_r = Y_r * (1/2*rho*V0*surface_wing_ideal*b)
     L_r = L_r * (1/2*rho*V0*surface_wing_ideal*b**2)
@@ -377,11 +384,11 @@ def r_der(sweep_quarter_wing,AR):
 ######P DERIVATIVES 
 ##################################################################
 
-def p_der(sweep_quarter_wing,AR):
+def p_der():
     
-    Y_v = v_der(sweep_quarter_wing,AR)[0]
-    L_r = r_der(sweep_quarter_wing,AR)[1]
-    Y_p = 2*Y_v * (z_f * np.cos(alpha_e) - l_f * np.sin(alpha_e)/b)
+    Y_v = v_der()[0]
+    L_r = r_der()[1]
+    Y_p = 2*Y_v * (y_AC_tail * np.cos(alpha_e) - l_V * np.sin(alpha_e)/b)
     L_p = 2 * Y_v * (z_f/b)**2
     N_p = L_r
 
@@ -391,25 +398,23 @@ def p_der(sweep_quarter_wing,AR):
 
     return Y_p, L_p, N_p
 
-
 ##################################################################
 ######CONSTRUCTION OF THE A MATRIX FOR THE LATERAL MOTION
 ##################################################################
 
+def lat_dyn_stab(): 
 
-def lat_dyn_stab(i,d,Cl): 
+    Y_v = v_der()[3]
+    Y_p = p_der()[0]
+    Y_r = r_der()[0]
 
-    Y_v = v_der(sweep_quarter_wing,AR)[0]
-    Y_p = p_der(sweep_quarter_wing,AR)[0]
-    Y_r = r_der(sweep_quarter_wing,AR)[0]
+    L_v = v_der()[1]
+    L_p = p_der()[1]
+    L_r = r_der()[1]
 
-    L_v = v_der(sweep_quarter_wing,AR)[1]
-    L_p = p_der(sweep_quarter_wing,AR)[1]
-    L_r = r_der(sweep_quarter_wing,AR)[1]
-
-    N_v = v_der(sweep_quarter_wing,AR)[2]
-    N_p = p_der(sweep_quarter_wing,AR)[2]
-    N_r = r_der(sweep_quarter_wing,AR)[2]
+    N_v = v_der()[2]
+    N_p = p_der()[2]
+    N_r = r_der()[2]
 
     M_matrix = np.array([
     [m, 0, 0, 0, 0],
@@ -419,7 +424,6 @@ def lat_dyn_stab(i,d,Cl):
     [0, 0, 0, 0, 1]
     ])
 
-
     B_matrix = np.array([
         [-Y_v, -(Y_p + m * W_e), -(Y_r - m * U_e), -m * g * np.cos(theta_e), -m * g * np.sin(theta_e)],
         [-L_v, -L_p, -L_r, 0, 0],
@@ -428,7 +432,6 @@ def lat_dyn_stab(i,d,Cl):
         [0, 0, -1, 0, 0]
     ])
 
-    # Calcul de la matrice A
     A_matrix = -np.linalg.inv(M_matrix) @ B_matrix
     eigenvalues = np.linalg.eigvals(A_matrix)
     real_parts = np.real(eigenvalues)
@@ -442,7 +445,7 @@ def lat_dyn_stab(i,d,Cl):
     else:
         test = 0
     
-    return real_parts, test
+    return real_parts, test, eigenvalues
 
 ##################################################################
 ######ANALYSIS OF THE PHUGOID MODE
@@ -459,12 +462,12 @@ def phugoid():
 ######SHORT PERIOD OSCIELLATIONS MODE
 ##################################################################
 
-def short_period(i,d,Cl):
+def short_period():
 
-    M_q = q_der2(config,fuel)[2]
-    M_w = w_der(config,fuel)[2]
-    Z_w = w_der(config,fuel)[0]
-    M_w_dot = w_dot_der(config,fuel)[2]
+    M_q = q_der2()[2]
+    M_w = w_der()[2]
+    Z_w = w_der()[0]
+    M_w_dot = w_dot_der()[2]
 
     omega_s = np.sqrt((M_q/I_y)*(Z_w/m)-(M_w/I_y)*U_e)
     xi_s = -(M_q/I_y + Z_w/m + M_w_dot/I_y*U_e)/(2*omega_s)
@@ -476,10 +479,15 @@ def short_period(i,d,Cl):
 
 def main(): 
 
-    real_parts_long, test_long = long_dyn_stab(config,fuel,0.45)
-    real_parts_lat, test_lat = lat_dyn_stab(config,fuel,0.45)
+    real_parts_long, test_long, eigenvalues_long = long_dyn_stab()
+    real_parts_lat, test_lat, eigenvalues_lat = lat_dyn_stab()
     omega_p, xi_p = phugoid()
-    omega_s, xi_s = short_period(config,fuel,0.45)
+    omega_s, xi_s = short_period()
+
+    print("--------------------------EIGENVALUES--------------------------------------------")
+    print("The eigenvales for the longitudinal motion are: ", eigenvalues_long)
+    print("The eigenvales for the lateral motion are: ", eigenvalues_lat)
+    print("----------------------------------------------------------------------")
 
     print("--------------------------CHECK OF THE SYSTEM--------------------------------------------")
     if test_long == 1:
@@ -496,7 +504,7 @@ def main():
         print("Thus, there is a problem with the definition of the system. The system is not well defined.")
     else: 
         print("Thus, the system is well defined.")
-
+    
     print("----------------------------------------------------------------------")
 
     print("--------------------------STABILITY--------------------------------------------")
@@ -507,14 +515,14 @@ def main():
     elif np.any(real_parts_long == 0):
         print("The system is dynamically neutrally stable for the longitudinal motion.")
     
-    if np.all(real_parts_long < 0):
+    if np.all(real_parts_lat < 0):
         print("The aircraft is dynamically stable for the lateral motion.")
-    elif np.any(real_parts_long > 0):
+    elif np.any(real_parts_lat > 0):
         print("The system is dynamiccaly unstable for the lateral motion.")
-    elif np.any(real_parts_long == 0):
+    elif np.any(real_parts_lat == 0):
         print("The system is dynamically neutrally stable for the lateral motion.")
     print("----------------------------------------------------------------------")
-
+    
     print("--------------------------PHUGOID MODE--------------------------------------------")
     print("The frequency of the phugoid mode is: ", omega_p, "rad/s")
     print("The damping ratio of the phugoid mode is: ", xi_p)
